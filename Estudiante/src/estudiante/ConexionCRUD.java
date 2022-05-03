c53f63574085a72e1e58108f7e8597ea2a418a4c
Guadalupe2022 <penafloresmorenaguadalupe@gmail.com>
2022-05-03 14:57:29 -0600

    Mi parte de la Evaluación terminada

--- Estudiante/src/estudiante/ConexionCRUD.java
+++ Estudiante/src/estudiante/ConexionCRUD.java
@@ -1,7 +1,55 @@
 
 package estudiante;
 
+import java.sql.*;
 
 public class ConexionCRUD {
+    
+    // Morena Guadalupe Peña Flores
+    public void guardaeRegistros(String tabla, String camposTabla, String valoresCampos) {
+        
+        ConexionCRUD conectar = new ConexionCRUD();
+        Connection cone = conectar.getConnection();
+        try{
+            
+            String sqlQueryStmt = "INSERT INFO " + tabla + " (" + camposTabla + ") VALUE (" +valoresCampos + "); ";
+            
+            Statement stmt;
+            stmt = cone.createStatement();
+            stmt.executeUpdate(sqlQueryStmt);
+            
+            stmt.close();
+            stmt.close();
+            System.out.println("Registro guadado correctamente!");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+                       
+        }  
+    }
+    
+    // Morena Guadalupe Peña Flores
+    public void actualizarEliminarRegistro(String tabla, String valoresCamposNuevos, String condicion){
+        
+        ConexionCRUD conectar = new ConexionCRUD();
+        Connection cone = conectar.getConnection();
+        try{
+            Statement stmt;
+            String sqlQueryStmt;
+            
+            if(valoresCamposNuevos.isEmpty()){
+                sqlQueryStmt = "DELETE FROM " + " WHERE " + condicion + ";";
+            }else{
+              sqlQueryStmt = " UPDATE " +tabla + " SET " + valoresCamposNuevos + " WHERE " + condicion +";";
+            }
+            stmt = cone.createStatement();
+            stmt.executeUpdate(sqlQueryStmt);
+            stmt.close();
+            stmt.close();
+        }catch(SQLException ex) {
+            System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage());
+            
+            
+        }
+    }
     
 }
--- Estudiante/src/estudiante/Delete.java
+++ Estudiante/src/estudiante/Delete.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estudiante;
 
-/**
- *
- * @author ITCA
- */
+import java.sql.SQLException;
+import java.util.Scanner;
+ 
+// Morena Guadalupe Peña Flores
 public class Delete {
+    
+    Delete() throws SQLException {
+        Scanner leer = new Scanner(System.in);
+        ConexionCRUD utilerias = new ConexionCRUD();
+        System.out.println("<< ELIMINAR REGISTRO >> ");
+        
+        System.out.println("Ingresar el id del registro: ");
+        String idEstudianteEliminar = leer.next();
+        
+        String tabla = "tb_estudiante";
+        String campos = "*";
+        String condicion = "id_estudiante = " + idEstudianteEliminar;
+        utilerias.desplegarRegistros(tabla, campos, condiciion);
+        
+        System.out.println("Presionar << Y >> para confirmar");
+        String confirmarBorrar = leer.next();
+        
+        if ("Y". equals(confirmarBorrar)){
+            
+            String valoresCamposNuevos = "";
+            
+            utilerias.actualizarEliminarRegistro(tabla, valoresCamposNuevos, condicion);
+            System.out.println("Registro borrado satisfactoriamente!");
+        }
+        
+        MenuPrincipal.desplegarmMenu();
+        
+    }
     
 }
