21df8514e8034e3b9519e49346cececf90a9017e
GerardoM3 <gerardomonroy13@gmail.com>
2022-05-03 21:01:39 -0600

    Mi parte de la evaluación terminada

--- Estudiante/src/estudiante/Update.java
+++ Estudiante/src/estudiante/Update.java
@@ -2,6 +2,9 @@
 package estudiante;
 import java.sql.SQLException;
 import java.util.Scanner;
+
+//Gerardo Misael Monroy Moza
+
 public class Update {
     Update() throws SQLException{
         Scanner leer = new Scanner(System.in);
@@ -17,16 +20,24 @@ public class Update {
         String condicionBuscar = "id_estudiante = " + est.getIdEstudiante();
         utilerias.desplegarRegistros(tablaBuscar, camposBuscar, condicionBuscar);
         
+        System.out.println("Carnet del alumno: ");
+        est.setCarnetEstudiante(leer.next());
+        
         System.out.println("Nombre del alumno: ");
         est.setNombreEstudiante(leer.next());
         
         System.out.println("Apellidos del estudiante: ");
-        est.setApellidoEstudiante();
+        est.setApellidoEstudiante(leer.next());
         
         System.out.println("Edad del estudiante: ");
-        est.setEdadEstudiante();
+        est.setEdadEstudiante(leer.next());
         
         String tabla = "tb_estudiante";
-        String camposValoresNuevos
+        String camposValoresNuevos = "carnet = '" + est.getCarnetEstudiante + "nombre = '" + est.getNombreEstudiante() + "', apellido = '" + est.getApellidoEstudiante() + "', edad = '" + est.getEdadEstudiante() + "'";
+        utilerias.actualizarEliminarRegistro(tabla, camposValoresNuevos, condicionBuscar);
+        
+        System.out.println("Modificado correctamente!");
+        
+        MenuPrincipal.desplegarMenu();
     }
 }
